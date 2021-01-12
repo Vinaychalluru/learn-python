276ac62f05fb85375766aef6460bfd13f9b03b1b
Vinay C. Kumar <vinay.c@osdc.ffi.co.in>
2021-01-12 12:59:03 +0530

    Untrack and Git Ignore

--- 2-others/django/v_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\practise\python\learn-python\2-others\django\v_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
